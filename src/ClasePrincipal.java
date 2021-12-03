9a7a427471d7e3e78a0727a1087aa35880caa379
Francisco Garache <franciscogarach3@gmail.com>
2021-12-03 11:54:35 -0600

    Initial commit

---
+++ src/ClasePrincipal.java
@@ -0,0 +1,13 @@
+import clases.Proceso1;
+import clases.Proceso2;
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        Proceso1 hilo1 = new Proceso1();
+        Thread hilo2 = new Thread(new Proceso2());
+
+        hilo1.start();
+        hilo2.start();
+    }
+
+}
